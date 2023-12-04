a70bec29fd0b001a2bf6537e2e534db8777f361f
backmat30 <matthewbackman@outlook.com>
2023-12-04 13:08:21 -0600

    initial commit

---
+++ src/edu/cs2430/assignment5/Suit.java
@@ -0,0 +1,19 @@
+package edu.cs2430.assignment5;
+
+/**
+ * enum for the card suits
+ */
+public enum Suit {
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES;
+
+    /**
+     * gets the string representation of the suit
+     * @return returns the string representation of the suit
+     */
+    public String toString(){
+
+    }
+}
