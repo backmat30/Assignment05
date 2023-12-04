8a339468298eea408014647487f0e2c3db98d2cd
backmat30 <matthewbackman@outlook.com>
2023-12-04 16:00:31 -0600

    initial commit

---
+++ src/edu/cs2430/assignment5/Game41Utils.java
@@ -0,0 +1,42 @@
+package edu.cs2430.assignment5;
+
+import java.util.Map;
+
+/**
+ * class containing utility methods for the game 41
+ */
+public class Game41Utils {
+    /**
+     * maps the ranks to their corresponding point values
+     */
+    private static Map<Rank, Integer> rankToPointMap;
+
+    /**
+     * calculates the point value for a given hand
+     * @param hand the hand being totalled
+     * @return returns points for the given hand
+     */
+    public static int calculateHandValue(CardGroup hand){
+
+    }
+
+    /**
+     * gets the point value associated with a given rank
+     * @param rank the given rank
+     * @return returns the point value for the rank
+     */
+    public static int getPointValue(Rank rank){
+
+    }
+    
+    /**
+     * determines if the round is over
+     * @param deck current deck of cards
+     * @param knockIndex index of the player who knocked
+     * @param currentTurnIndex index of the current turn
+     * @return returns true if the round is over, otherwise false
+     */
+    public static boolean isHandOver(CardGroup deck, int knockIndex, int currentTurnIndex){
+
+    }
+}
