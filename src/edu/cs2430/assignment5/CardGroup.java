cfcefff50b1b37a09a0b315bb79dbb0e350592ff
backmat30 <matthewbackman@outlook.com>
2023-12-04 18:37:55 -0600

    fixed shuffle loop starting out of bounds

--- src/edu/cs2430/assignment5/CardGroup.java
+++ src/edu/cs2430/assignment5/CardGroup.java
@@ -1,4 +1,5 @@
 package edu.cs2430.assignment5;
+
 import java.util.List;
 import java.util.Random;
 import java.lang.reflect.Array;
@@ -14,50 +15,54 @@ public class CardGroup {
     /**
      * default constructor, sets cardList to an empty list
      */
-    public CardGroup(){
+    public CardGroup() {
         cardList = new ArrayList<>();
     }
 
     /**
      * copy constructor
+     * 
      * @param otherGroup cardGroup to be copied from
      */
-    public CardGroup(CardGroup otherGroup){
-        for(int i = 0; i < otherGroup.size(); i++){
+    public CardGroup(CardGroup otherGroup) {
+        for (int i = 0; i < otherGroup.size(); i++) {
             cardList.add(otherGroup.getCard(i));
         }
     }
 
     /**
      * adds a card to the group
+     * 
      * @param card card to be added
      */
-    public void addCard(Card card){
+    public void addCard(Card card) {
         cardList.add(card);
     }
 
     /**
      * clears the card list
      */
-    public void clear(){
+    public void clear() {
         cardList = new ArrayList<>();
     }
 
     /**
      * gets the card at the specified index
+     * 
      * @param index index of card
      * @return returns card at the index given
      */
-    public Card getCard(int index){
+    public Card getCard(int index) {
         return cardList.get(index);
     }
 
     /**
      * gets the last card in the group
+     * 
      * @return returns the last card in the group
      */
-    public Card getLastCard(){
-        if(cardList.size() == 0){
+    public Card getLastCard() {
+        if (cardList.size() == 0) {
             return null;
         }
         return cardList.get(cardList.size() - 1);
@@ -65,27 +70,30 @@ public class CardGroup {
 
     /**
      * determines if the group is empty
+     * 
      * @return returns true if empty otherwise false
      */
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return cardList.size() == 0;
     }
 
     /**
      * removes a card at the given index
+     * 
      * @param index index to remove card at
      * @return returns the removed card
      */
-    public Card removeCard(int index){
+    public Card removeCard(int index) {
         return cardList.remove(index);
     }
 
     /**
      * removes the last card in the group
+     * 
      * @return returns the removed card
      */
-    public Card removeLastCard(){
-        if(cardList.size() == 0){
+    public Card removeLastCard() {
+        if (cardList.size() == 0) {
             return null;
         }
         return cardList.remove(cardList.size() - 1);
@@ -93,10 +101,11 @@ public class CardGroup {
 
     /**
      * shuffles the group of cards
+     * 
      * @param random random object to use for shuffling
      */
-    public void shuffle(Random random){
-        for(int i = cardList.size(); i > 0; i--){
+    public void shuffle(Random random) {
+        for (int i = cardList.size() - 1; i > 0; i--) {
             Card currentCard = cardList.get(i);
             int swapIndex = random.nextInt(i);
             cardList.set(i, cardList.get(swapIndex));
@@ -106,24 +115,26 @@ public class CardGroup {
 
     /**
      * gets the size of the group
+     * 
      * @return returns the size of the card list
      */
-    public int size(){
+    public int size() {
         return cardList.size();
     }
 
     /**
      * sorts the card group
      */
-    public void sort(){
+    public void sort() {
         cardList.sort(null);
     }
 
     /**
      * gets the string representation of the card group
+     * 
      * @return returns the string representation of the card group
      */
-    public String toString(){
+    public String toString() {
         String output = "";
         for (Card card : cardList) {
             output += card.toString() + " ";
@@ -156,5 +167,4 @@ public class CardGroup {
         return true;
     }
 
-    
 }
