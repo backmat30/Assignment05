6734376a547628341cc30cb56b578e467d8fdc03
backmat30 <matthewbackman@outlook.com>
2023-12-04 19:19:09 -0600

    changed shuffle algorithm

--- src/edu/cs2430/assignment5/CardGroup.java
+++ src/edu/cs2430/assignment5/CardGroup.java
@@ -105,7 +105,7 @@ public class CardGroup {
      * @param random random object to use for shuffling
      */
     public void shuffle(Random random) {
-        for (int i = 0; i < cardList.size(); i++) {
+        for (int i = cardList.size() - 1; i >= 0; i--) {
             Card currentCard = cardList.get(i);
             int swapIndex = random.nextInt(i+1);
             cardList.set(i, cardList.get(swapIndex));
